b26329645214b3967cf73d555600604a6f0583d0
Katie Pundt <kzpundt@gmail.com>
2022-03-18 20:35:21 -0700

    render add project form

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
     this.attach();
   }
 
@@ -16,3 +17,5 @@ class ProjectInput {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
+
+const projInput = new ProjectInput();
